c84ba0bfde512f6e349097c978483482cc97ded4
Erdinç Cürebal <erdinccurebal@hotmail.com>
2020-12-16 15:02:05 +0300

    init

---
+++ browser/js/main.js
@@ -0,0 +1,15 @@
+// ! Body içerisinden etiketleri ve id'leri yakalayalım.
+// document.querySelector("#test").innerHTML = "<h1></h1><p></p><small></small>";
+// document.querySelector("#test>h1").innerHTML = "Practice";
+// document.querySelector("#test>p").innerHTML = "Lorem ipsun";
+// document.querySelector("#test>small").innerHTML = "smalcuk";
+// document.querySelectorAll("p").forEach(e => e.style.fontSize = "35px");
+
+// ! Body bir içerik yazdır.
+// document.write("Hello World");
+// document.write("Hello World");
+// document.write("Hello World");
+// document.write("Hello World");
+
+// ! Uyarı at.
+// alert("Hello");
---
+++ nodejs/index.js
@@ -0,0 +1,2 @@
+process.stdout.write("new line\n");
+
